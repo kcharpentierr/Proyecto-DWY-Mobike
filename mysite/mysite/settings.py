ae99b0b011dad318659c473fcc3e4528661986f6
kcharpentierr <kcharpentierr@gmail.com>
2020-11-12 19:20:51 -0300

    Carpeta static

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -125,5 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'images')
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'static-only')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'mobike.chile.duoc@gmail.com'
+EMAIL_HOST_PASSWORD = 'mobike123'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
