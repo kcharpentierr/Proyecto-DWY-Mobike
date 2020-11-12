4fced7bd145523d22e4ade33eb0db1f7e540fefe
kcharpentierr <kcharpentierr@gmail.com>
2020-11-12 19:55:31 -0300

    Creación ejecutivo

--- mysite/apps/Ejecutivo/models.py
+++ mysite/apps/Ejecutivo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Ejecutivo (models.Model):
+    fotografia = models.ImageField(upload_to='ejecutivos')
+    rut = models.CharField(max_length=15)
+    nombre = models.CharField(max_length=50)
+    email = models.EmailField(
+        max_length=70, blank=True, null=True, unique=True)
+
+
+def _str_(self):
+    return str(self.fotografia)
