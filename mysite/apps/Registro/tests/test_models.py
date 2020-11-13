e4cfca947d5ab15e539a8fe35ea68ef26d571102
Sebastian Hormazabal <sebaa.ignacio23@gmail.com>
2020-11-13 00:56:18 -0300

    pruebas unitarias

---
+++ mysite/apps/Registro/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.template.defaultfilters import slugify
+from Registro.models import Portico, Bicicleta
+from Registro.forms import Portico, Bicicleta
+
+class PorticoTestCase(TestCase):
+    def setUp(self):
+        Portico.objects.create(id_portico=11111, ubicacion= "AAAAAA")
+        Portico.objects.create(id_portico=22222, ubicacion= "BBBBBB")
+
+    def test_ingresar_porticos(self):
+        """Los porticos se registran correctamente en la BD"""
+        Portico_1 = Portico.objects.get(id_portico=11111)
+        Portico_2 = Portico.objects.get(id_portico=22222)
+        self.assertEqual(Portico_1.ubicacion, "AAAAAA")
+        self.assertEqual(Portico_2.ubicacion, "BBBBBB")
